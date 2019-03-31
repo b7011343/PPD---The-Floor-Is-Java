62c1ca8107a209be2d651087b273acb4ff9ecbcc
Jacob Whitehead <b7011343@my.shu.ac.uk>
2019-03-31 19:20:52 +0100

    level 1 implementation (may need more work)

--- PPD Game Assignment/p5/website/js/LevelSelector.js
+++ PPD Game Assignment/p5/website/js/LevelSelector.js
@@ -33,15 +33,28 @@ function LevelSelector(level){
             }
             case 1:     // LEVEL 1 (JACOB)
             {
-                
-                this.endX = 200
-                this.endY = 300
+                this.endX = 4900
+                this.endY = 475
                 endFlag = new EndFlag(this.endX, this.endY)
-                var plat1 = new Platform(-1280, 300, 50, 1600)
-                
+                var plat1 =  new Platform(-1280, 300, 50, 1600)
+                var plat2 =  new Platform(580, 300, 50, 100)
+                var plat3 =  new Platform(960, 300, 50, 100)
+                var plat4 =  new Platform(1335, 300, 50, 100)
+                var plat5 =  new Platform(1620, 250, 50, 100)
+                var plat6 =  new Platform(1860, 200, 50, 100)
+                var plat7 =  new Platform(2260, 300, 50, 400)
+                var plat8 =  new Platform(3100, 600, 50, 250)
+                var plat9 =  new Platform(3490, 550, 50, 100)
+                var plat10 = new Platform(3730, 500, 50, 100)
+                var plat11 = new Platform(4050, 500, 50, 10)
+                var plat12 = new Platform(4280, 500, 50, 10)
+                var plat13 = new Platform(4510, 500, 50, 10)
+                var plat14 = new Platform(4750, 475, 50, 350)
+
                 this.message = "-LEVEL 1-"
 
-                platformList = [plat1]
+                platformList = [plat1, plat2, plat3, plat4, plat5, plat6, plat7, plat8, plat9, plat10,
+                                plat11, plat12, plat13, plat14]
                 break;
             }
             case 2:     // LEVEL 2 (JOE)
